58837aaedee0c15cff3767516f13a06cb16126e7
Yevheniy Lemak <zhenualemak@gmail.com>
2025-07-16 20:07:41 +0300

    fix: interval and dependencies array in useEffect

--- src/hooks/useLiveClock.js
+++ src/hooks/useLiveClock.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useWeather } from "../context/WeatherContext";
 
 // Return live time in selected city based on user browser time and openweather api timezone
@@ -6,23 +6,23 @@ export const useLiveClock = () => {
   const { localWeatherData } = useWeather();
   const timezoneOffset = localWeatherData?.timezone ?? 0;
 
-  const getLocalTime = () => {
+  const getLocalTime = useCallback(() => {
     const utcNow = Date.now();
     const localTimeMs = utcNow + timezoneOffset * 1000;
     const date = new Date(localTimeMs);
     const hours = date.getUTCHours().toString().padStart(2, "0");
     const minutes = date.getUTCMinutes().toString().padStart(2, "0");
     return `${hours}:${minutes}`;
-  };
+  }, [timezoneOffset]);
 
-  const [liveTime, setLiveTime] = useState(getLocalTime);
+  const [liveTime, setLiveTime] = useState(getLocalTime());
 
   useEffect(() => {
     const interval = setInterval(() => {
       setLiveTime(getLocalTime());
     }, 1000);
-    return clearInterval(interval);
-  }, [timezoneOffset]);
+    return () => clearInterval(interval);
+  }, [getLocalTime]);
 
   return liveTime;
 };
