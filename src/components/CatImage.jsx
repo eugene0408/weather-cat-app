276043fd1543def6c613aab9ff6e14c5a71cc2b8
Yevheniy Lemak <zhenualemak@gmail.com>
2025-08-30 18:03:20 +0300

    add new and replace some old cat images

--- src/components/CatImage.jsx
+++ src/components/CatImage.jsx
@@ -2,12 +2,12 @@ const mapCatImage = (weather) => {
   const temp = Math.round(weather.main.temp);
   const main = weather.weather[0].main;
 
-  if (["Rain", "Drizzle"].includes(main)) return "rain";
-  if (main === "Thunderstorm") return "storm";
+  if (["Rain", "Drizzle", "Thunderstorm"].includes(main)) return "rain";
+  if (["Mist", "Fog"].includes(main)) return "fog";
   if (main === "Clouds") return "cloudy";
   if (temp < 5) return "cold";
-  if (temp < 25) return "sunny1";
-  if (temp >= 25) return "sunny2";
+  if (temp < 25) return "sunny";
+  if (temp >= 25) return "hot";
 };
 
 export const CatImage = ({ weather, size }) => {
