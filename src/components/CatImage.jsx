54286880db141b80634b27e59968a7fa0e0e779c
Yevheniy Lemak <zhenualemak@gmail.com>
2025-09-09 18:20:04 +0300

    change some images and new, chaged dark theme background color

--- src/components/CatImage.jsx
+++ src/components/CatImage.jsx
@@ -2,13 +2,26 @@ const mapCatImage = (weather) => {
   const temp = Math.round(weather.main.temp);
   const main = weather.weather[0].main;
 
-  if (["Rain", "Drizzle", "Thunderstorm"].includes(main)) return "rain";
-  if (["Mist", "Smoke", "Haze", "Fog"].includes(main)) return "fog";
-  if (main === "Clouds") return "cloudy";
-  if (temp < 0 && temp > -5) return "cold";
-  if (temp < -5) return "cold2";
-  if (temp < 25) return "sunny";
-  if (temp >= 25) return "hot";
+  switch (true) {
+    case ["Rain", "Drizzle", "Thunderstorm"].includes(main):
+      return "rain";
+    case ["Mist", "Smoke", "Haze", "Fog"].includes(main):
+      return "fog";
+    case main === "Clouds":
+      return "cloudy";
+    case temp < 0 && temp > -5:
+      return "cold";
+    case temp < -5 && temp >= -10:
+      return "cold2";
+    case temp < -10:
+      return "cold3";
+    case temp < 25:
+      return "sunny";
+    case temp >= 25:
+      return "hot";
+    default:
+      return "sunny";
+  }
 };
 
 export const CatImage = ({ weather, size }) => {
